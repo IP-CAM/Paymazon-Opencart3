e7bc84f494bb6dcda1934c44f094af30e60e9b92
Imran Nababan <imzers@gmail.com>
2018-01-11 17:56:32 +0700

    Add files via upload

---
+++ admin/language/en-gb/extension/payment/paymazonkbankinst42.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']      	= 'Paymazon With KBank Installment (ShopID = 42)';
+
+// Text
+$_['text_extension']	 	= 'Extensions';
+$_['text_payment']       	= 'Payment';
+$_['text_success']      	= 'Success: You have modified Paymazon KBank Installment configuration!';
+$_['text_paymazonkbankinst42']	= '<a href="https://payment.paymazon.com" target="_blank"><img src="view/image/payment/paymazon.png" width="120px" alt="Paymazon" title="Paymazon Kbank" style="border: 1px solid #778899;" /></a>';
+$_['text_live']          	= 'live';
+$_['text_sandbox']       	= 'sandbox';
+$_['text_successful']    	= 'Always Successful';
+$_['text_fail']          	= 'Always Fail';
+$_['text_edit']          	= 'Configure Paymazon KBank';
+
+// Entry
+$_['entry_environment']  	= 'Environment';
+$_['entry_merchant_id']  	= 'Merchant Id';
+$_['entry_shared_key']   	= 'Shared Key';
+$_['entry_order_status'] 	= 'Order Status';
+$_['entry_oneclick']     	= 'One click';
+$_['entry_geo_zone']     	= 'Geo Zone';
+$_['entry_status']       	= 'Status';
+$_['entry_sort_order']   	= 'Sort Order:';
+$_['entry_expiry']   	 	= 'Custom Expire';
+$_['entry_custom_field'] 	= 'Put Custom Field, For Ref1, Ref2, Ref3';
+$_['entry_minimum_total']	= 'Total minimum for using this payment gateway.';
+$_['entry_currency_conversion'] 	= 'Currency conversion to THB';
+$_['entry_paymazonkbankinst42_success_mapping'] 		= 'Map Payment Success Status to Order Status:';
+$_['entry_paymazonkbankinst42_challenge_mapping'] 	= 'Map Payment Challenge Status to Order Status:';
+$_['entry_paymazonkbankinst42_failure_mapping'] 		= 'Map Payment Failure Status to Order Status:';
+$_['entry_display_name'] 	= 'Display name:';
+$_['entry_shopids_value']	= "Value of ShopID";
+$_['entry_shopids_text']	= "Text of ShopID";
+$_['entry_shopids_terms']	= "Installment Terms of ShopID";
+
+// Error
+$_['error_permission']   			= 'Warning: You do not have permission to modify Paymazon KBank!';
+$_['error_shared_key']   			= 'Shared Key is required....';
+$_['error_currency_conversion'] 	= 'Currency conversion rate is required when THB currency is not installed in the system!';
+$_['error_display_name'] 			= 'Please specify a name for this payment method!';
+?>
