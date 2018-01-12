0c3af7ed0cd0da284ab56aa03d2237686465afff
Imran Nababan <imzers@gmail.com>
2018-01-12 10:03:01 +0700

    Separate installment by shopid

---
+++ admin/language/th-th/extension/payment/paymazonkbank.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title']      = 'Paymazon KBank';
+
+// Text
+$_['text_extension']	 = 'Extensions';
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified Paymazon KBank configuration!';
+$_['text_snap']     	 = '<a href="https://payment.paymazon.com" target="_blank"><img src="view/image/payment/paymazon.png" width="120px" alt="Paymazon" title="Paymazon Kbank" style="border: 1px solid #778899;" /></a>';
+$_['text_live']          = 'production';
+$_['text_sandbox']       = 'sandbox';
+$_['text_successful']    = 'Always Successful';
+$_['text_fail']          = 'Always Fail';
+$_['text_edit']          = 'Configure Paymazon KBank';
+
+// Entry
+$_['entry_environment']  = 'Environment'; // v2 API only
+$_['entry_merchant_id']  = 'Merchant Id'; // v2 API only
+$_['entry_client_key']   = 'Client Key'; // v2 API only
+$_['entry_server_key']   = 'Server Key'; // v2 API only
+$_['entry_order_status'] = 'Order Status';
+$_['entry_oneclick']     = 'One click';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_expiry']   	 = 'Custom Expire';
+$_['entry_custom_field'] = 'Custom Field';
+$_['entry_currency_conversion'] = 'Currency conversion to THB';
+$_['entry_snap_success_mapping'] = 'Map Payment Success Status to Order Status:';
+$_['entry_snap_challenge_mapping'] = 'Map Payment Challenge Status to Order Status:';
+$_['entry_snap_failure_mapping'] = 'Map Payment Failure Status to Order Status:';
+$_['entry_display_name'] = 'Display name:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify Paymazon KBank!';
+$_['error_client_key']   = 'Client Key is required!';
+$_['error_server_key']   = 'Server Key is required!';
+$_['error_currency_conversion'] = 'Currency conversion rate is required when THB currency is not installed in the system!';
+$_['error_display_name'] = 'Please specify a name for this payment method!';
+?>
